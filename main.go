0fe17b6ff5e3a6732f0aab4105f90f847affa26e
Jordan Abderrachid <jabderrachid@gmail.com>
2016-01-14 17:50:55 +0100

    Start implementing CLI interface.

--- main.go
+++ main.go
@@ -1,3 +1,28 @@
 package main
 
-func main() {}
+import (
+	"flag"
+	"github.com/jordanabderrachid/go-chip8/cpu"
+	"github.com/nsf/termbox-go"
+	"os"
+)
+
+func main() {
+	romFile := flag.String("r", "", "rom file")
+	flag.Parse()
+
+	if err := termbox.Init(); err != nil {
+		panic(err)
+	}
+	defer termbox.Close()
+	termbox.SetInputMode(termbox.InputEsc)
+
+	CPU := new(cpu.CPU)
+	CPU.Reset()
+
+	f, _ := os.Open(*romFile)
+	b := make([]byte, 3584)
+	f.Read(b)
+	CPU.LoadData(b)
+	CPU.Run()
+}
